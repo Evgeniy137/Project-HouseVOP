ff664b19222e401b6e9414d94a606a564dd7f6f9
Evgeniy137 <vavilon11111111@gmail.com>
2020-02-26 18:40:59 +0200

    блок с галереей и слайдером

--- app/js/scripts/popup.js
+++ app/js/scripts/popup.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
         type:'inline',
         closeBtnInside:'true',
         enablescapekey:'true',
+        removalDelay: 300,
         callbacks: {
             open: function(){
                 $('.slider').slick();
